35c4a704224e32f85f36e0abf37e8e4b09fd0f4d
Khizer Butt <99053182@mydavinci.nl>
2018-11-30 16:40:46 +0100

    .

--- adventuregame-template/adventuregame-template/adventure.js
+++ adventuregame-template/adventuregame-template/adventure.js
@@ -10,13 +10,95 @@
 	var modalManualTextNode=document.createTextNode("In de verhaal moet je een spel spelen. Je kan kiezen tussen Easy, Medium en Hard difficulty. Bij iedere difficulty heb je andere tijden en moeilijkheden om de level te halen. Je krijgt ook punten die afhankelijk zijn van de difficulty.");
 	var sceneOneText=document.createTextNode("Er was is een Henk. Hij was dik, lang en verlegen. Hij wou even naar buiten om boodschappen te doen, hij kwam onderweg zijn buurman Koen tegen en zei hoi tegen hem.");
 	var sceneTwoText=document.createTextNode("Henk liep naar de supermarkt. Voor de supermarkt waren veel berenvallen voor geen reden. Henk was slechtziend dus zag het niet. Hij stapte op een val en zijn voet was afgehakt door de stekelige tanden van de val. Henk en zijn voet vielen allebij in een put naast hem.");
-	var sceneThreeText=document.createTextNode("In de put stond een pc met NVIDIA GeForce GTX 2080 ti, Intel Core i9 8000, een maxxter muis en een keyboard, maar het had wel gare speakers. En het had een 4k display 140hz hdr etc.");
+	var sceneThreeText=document.createTextNode("In de put stond stoel met een pc met NVIDIA GeForce GTX 2080 Ti, Intel Core i9 8000, een maxxter muis en een keyboard, maar het had wel gare speakers. En het had een 4k display 140hz HDR etc.");
+	var sceneFourText=document.createTextNode("Op die stoel is een proximity sensor, die ervoor zorgt dat de pc aangaat. Maar Henk was dik genoeg dat de sensor kapot ging maar de pc ging alsnog aan");
 
 //VarsMetLangeTexten
 
 
 
 //Functions
+
+	function prevButtonHoverLight(argument) {
+		
+		prevButton.style.width="400px";
+		prevButton.style.borderRadius="0px 50px 50px 0px";
+		prevButton.style.backgroundColor="black";
+		prevButton.style.color="white";
+		prevButton.style.borderColor="white";
+
+	}
+
+	function nextButtonHoverDark(argument) {
+		
+		nextButton.style.width="400px";
+		nextButton.style.borderRadius="50px 0px 0px 50px";
+		nextButton.style.backgroundColor="white";
+		nextButton.style.color="black";
+		nextButton.style.borderColor="black";
+
+	}
+
+	function nextButtonHoverLight(argument) {
+		
+		nextButton.style.width="400px";
+		nextButton.style.borderRadius="50px 0px 0px 50px";
+		nextButton.style.backgroundColor="black";
+		nextButton.style.color="white";
+		nextButton.style.borderColor="white";
+
+	}
+
+	function prevButtonHoverDark(argument) {
+		
+		prevButton.style.width="400px";
+		prevButton.style.borderRadius="0px 50px 50px 0px";
+		prevButton.style.backgroundColor="white";
+		prevButton.style.color="black";
+		prevButton.style.borderColor="black";
+
+	}
+
+	function changeButtonStyleLight(argument) {
+
+		nextButton.style.backgroundColor="white";
+		nextButton.style.color="black";
+		nextButton.style.borderColor="black";
+		nextButton.style.width="300px";
+
+		prevButton.style.backgroundColor="white";
+		prevButton.style.color="black";
+		prevButton.style.borderColor="black";
+		prevButton.style.width="300px";
+
+	}
+
+	function changeButtonStyleDark(argument) {
+		
+		nextButton.style.backgroundColor="#595959";
+		nextButton.style.color="white";
+		nextButton.style.borderColor="white";
+		nextButton.style.width="300px";
+
+		prevButton.style.backgroundColor="#595959";
+		prevButton.style.color="white";
+		prevButton.style.borderColor="white";
+		prevButton.style.width="300px";
+
+	}
+
+	function backgroundColorChange(color, bodyColor) {
+
+		gameContainer.style.backgroundColor=color;
+		document.body.style.backgroundColor=bodyColor;
+
+	}
+
+	function borderColorChange(color) {
+		
+		modalStartText.style.borderColor=color;
+
+	}
 
 	function confirm(argument) {
 
@@ -29,6 +111,7 @@
 		console.log("Started Game");
 		console.log("Scene 1");
 
+		document.body.style.transition="1s";
 		document.body.style.backgroundImage="url(img/situatie1.png)";
 		document.body.style.backgroundSize="cover";
 		document.body.style.backgroundRepeat="no-repeat";
@@ -53,12 +136,22 @@
 	function sceneTwo(argument) {
 
 		console.log("Scene 2");
-
 		document.body.style.backgroundImage="url(img/situatie2.png)";
+		changeButtonStyleDark();
+
 		prevButton.style.display="block";
 		
+		nextButton.setAttribute("onmousemove", "nextButtonHoverDark()");
+		prevButton.setAttribute("onmousemove", "prevButtonHoverDark()");
+
+		nextButton.setAttribute("onmouseleave", "changeButtonStyleDark()");
+		prevButton.setAttribute("onmouseleave", "changeButtonStyleDark()");
+		
+		prevButton.setAttribute("onclick", "sceneOne()");
 		nextButton.setAttribute("onclick", "sceneThree()");
-		prevButton.setAttribute("onclick", "sceneOne()")
+
+		sceneP.style.backgroundColor="#595959";
+		sceneP.style.color="white";
 
 		sceneP.innerHTML=sceneTwoText.textContent;
 
@@ -68,22 +161,19 @@
 
 		console.log("Scene 3");
 		document.body.style.backgroundImage="url(img/situatie3.png)";
-
+		changeButtonStyleLight();
 		
 		nextButton.setAttribute("onclick", "sceneFour()");
 		prevButton.setAttribute("onclick", "sceneTwo()");
 
+		nextButton.setAttribute("onmousemove", "nextButtonHoverLight()");
+		prevButton.setAttribute("onmousemove", "prevButtonHoverLight()");
+
+		nextButton.setAttribute("onmouseleave", "changeButtonStyleLight()");
+		prevButton.setAttribute("onmouseleave", "changeButtonStyleLight()");
+
 		sceneP.style.backgroundColor="white";
 		sceneP.style.color="black";
-		sceneP.style.borderColor="black";
-
-		nextButton.style.backgroundColor="white";
-		nextButton.style.color="black";
-		nextButton.style.borderColor="black";
-
-		prevButton.style.backgroundColor="white";
-		prevButton.style.color="black";
-		prevButton.style.borderColor="black";
 
 		sceneP.innerHTML=sceneThreeText.textContent;
 	}
@@ -95,6 +185,8 @@
 
 		nextButton.setAttribute("onclick", "")
 		prevButton.setAttribute("onclick", "sceneThree()")
+
+		sceneP.innerHTML=sceneFourText.textContent;
 
 	}
 
@@ -138,6 +230,8 @@
 	var gameTitle=document.createElement("p");
 	gameTitle.setAttribute("id", "gameTitle");
 	gameTitle.setAttribute("onclick", "confirm()");
+	gameTitle.setAttribute("onmousemove", "backgroundColorChange('#0000ff', '#000080')");
+	gameTitle.setAttribute("onmouseleave", "backgroundColorChange('#666666', '#404040')");
 
 		var modalStartText=document.createElement("p");
 		modalStartText.setAttribute("id", "modalStartText");
@@ -145,12 +239,18 @@
 	var storyBtnTitle=document.createElement("p");
 	storyBtnTitle.setAttribute("id", "storyBtnTitle");
 	storyBtnTitle.setAttribute("onclick", "showStoryText()");
+	storyBtnTitle.setAttribute("onmousemove", "backgroundColorChange('#800000', '#660000')");
+	storyBtnTitle.setAttribute("onmouseleave", "backgroundColorChange('#666666', '#404040')");
+
 		var modalStoryText=document.createElement("p");
 		modalStoryText.setAttribute("id", "modalStoryText");
 
 	var manualTitle=document.createElement("p");
 	manualTitle.setAttribute("id", "manualTitle");
 	manualTitle.setAttribute("onclick", "showManualText()");
+	manualTitle.setAttribute("onmousemove", "backgroundColorChange('#00cc00', '#145214')");
+	manualTitle.setAttribute("onmouseleave", "backgroundColorChange('#666666', '#404040')");
+
 		var modalManualText=document.createElement("p");
 		modalManualText.setAttribute("id", "modalManualText");
 
@@ -160,10 +260,14 @@
 	var modalStartClose=document.createElement("button");
 	modalStartClose.setAttribute("id", "modalStartClose");
 	modalStartClose.setAttribute("onclick", "modalStart.style.display='none';");
+	modalStartClose.setAttribute("onmousemove", "borderColorChange('#ff0000')")
+	modalStartClose.setAttribute("onmouseleave", "borderColorChange('black')")
 
 	var modalStartContinue=document.createElement("button");
 	modalStartContinue.setAttribute("id", "modalStartContinue");
 	modalStartContinue.setAttribute("onclick", "sceneOne()");
+	modalStartContinue.setAttribute("onmousemove", "borderColorChange('#00ff00')");
+	modalStartContinue.setAttribute("onmouseleave", "borderColorChange('black')");
 
 	var modalStoryClose=document.createElement("button");
 	modalStoryClose.setAttribute("id", "modalStoryClose");
